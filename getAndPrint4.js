c90e6df63c3cd3801054606525e4577df352f40d
Kyle Tycholiz <tycholiz22@hotmail.com>
2018-08-06 19:39:17 +0000

    third exercise

---
+++ getAndPrint4.js
@@ -0,0 +1,33 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+
+  var allData = "";
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      allData += data;
+    });
+
+    response.on('end', function() {
+      callback(allData);
+      console.log('Response stream complete.');
+    });
+
+  });
+
+}
+
+function printHTML (html) {  //this is the callback that gets passed through getHTML
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML);
